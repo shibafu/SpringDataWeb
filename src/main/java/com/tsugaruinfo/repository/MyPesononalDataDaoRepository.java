7394b865459632e6686d173777bd658dd2ec9ce4
shibafu <civaf-th123@hotmail.co.jp>
2018-03-15 18:00:21 +0900

    リポジトリ機能追加

--- src/main/java/com/tsugaruinfo/repository/MyPesononalDataDaoRepository.java
+++ src/main/java/com/tsugaruinfo/repository/MyPesononalDataDaoRepository.java
@@ -1,5 +1,7 @@
 package com.tsugaruinfo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,15 @@ import com.tsugaruinfo.model.Mypersonaldata;
 public interface MyPesononalDataDaoRepository
 	extends JpaRepository<Mypersonaldata, Integer> {
 
-}
+	public List<Mypersonaldata> findByName(String name);
+	public List<Mypersonaldata> findByMail(String mail);
+	public List<Mypersonaldata> findByAge(int age);
+	public List<Mypersonaldata> findByNameLike(String name);
+	public List<Mypersonaldata> findByMailLike(String mail);
+	public List<Mypersonaldata> findByNameOrMail(String name, String mail);
+	public List<Mypersonaldata> findByNameLikeOrMailLike(String name, String mail);
+	
+	public List<Mypersonaldata> findByAgeGreaterThan(Integer age);
+	public List<Mypersonaldata> findByAgeLessThan(Integer age);
+	public List<Mypersonaldata> findByAgeGreaterThanOrAgeLessThan(Integer age0, Integer age1);
+ }
